bbf7193fb68027d9c06a58a39ccad49ffe127579
ML510 <mr.mahesh.303@gmail.com>
2026-07-31 10:02:43 +0530

    Create Stage

--- src/main/java/Starter.java
+++ src/main/java/Starter.java
@@ -1,6 +1,29 @@
-public class Starter {
-    public static void main(String[] args) {
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import config.AppModule;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Starter extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Injector injector = Guice.createInjector(new AppModule());
+
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view/dashboard.fxml"));
+
+        fxmlLoader.setControllerFactory(injector::getInstance);
+
+
+        stage.setScene(new Scene(fxmlLoader.load()));
+        stage.show();
     }
 }
 
