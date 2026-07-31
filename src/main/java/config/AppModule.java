f9df0e1cea7f46a905248d13b892576b8e6a8909
ML510 <mr.mahesh.303@gmail.com>
2026-07-31 13:29:47 +0530

    Update App Module

--- src/main/java/config/AppModule.java
+++ src/main/java/config/AppModule.java
@@ -1,12 +1,32 @@
 package config;
 
 import com.google.inject.AbstractModule;
+import repository.custom.*;
+import repository.custom.impl.*;
+import service.custom.*;
+import service.custom.impl.*;
 
 public class AppModule extends AbstractModule {
 
     @Override
     protected void configure(){
+        bind(CustomerService.class).to(CustomerServiceImpl.class);
+        bind(CustomerDao.class).to(CustomerDaoImpl.class);
 
+        bind(EmployeeService.class).to(EmployeeServiceImpl.class);
+        bind(EmployeeDao.class).to(EmployeeDaoImpl.class);
+
+        bind(ItemService.class).to(ItemServiceImpl.class);
+        bind(ItemDao.class).to(ItemDaoImpl.class);
+
+        bind(OrdersService.class).to(OrderServiceImpl.class);
+        bind(OrderDao.class).to(OrderDaoImpl.class);
+
+        bind(SupplierService.class).to(SupplierServiceImpl.class);
+        bind(SupplierDao.class).to(SupplierDaoImpl.class);
+
+        bind(OrderDetailsService.class).to(OrderDetailsServiceImpl.class);
+        bind(OrderDetailsDao.class).to(OrderDetailDaoImpl.class);
 
     }
 }
