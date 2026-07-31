5ba692f7394829d06b394367b45822b59e2d9a54
ML510 <mr.mahesh.303@gmail.com>
2026-07-31 10:01:20 +0530

    Hibernate Config

---
+++ src/main/java/config/HibernateConfig.java
@@ -0,0 +1,29 @@
+package config;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.model.naming.ImplicitNamingStrategyJpaCompliantImpl;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateConfig {
+
+    private static SessionFactory sessian = createSession();
+
+    private static SessionFactory createSession() {
+
+        StandardServiceRegistry build = new StandardServiceRegistryBuilder().configure("hibernate.cfg.xml").build();
+
+        Metadata metadata = new MetadataSources(build)
+
+                .getMetadataBuilder()
+                .applyImplicitNamingStrategy(ImplicitNamingStrategyJpaCompliantImpl.INSTANCE).build();
+
+        return metadata.getSessionFactoryBuilder().build();
+    }
+    public static Session geSession(){
+        return sessian.openSession();
+    }
+}
