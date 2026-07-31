acc42e2ceb441490e471d5f3ffc630e500446ad0
ML510 <mr.mahesh.303@gmail.com>
2026-07-31 12:16:13 +0530

    Create Admin Register From

---
+++ src/main/java/controller/admin/AdminRegisterFrom.java
@@ -0,0 +1,20 @@
+package controller.admin;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.fxml.FXML;
+
+public class AdminRegisterFrom {
+
+    @FXML
+    private JFXTextField txtCPasswoed;
+
+    @FXML
+    private JFXTextField txtEmail;
+
+    @FXML
+    private JFXTextField txtName;
+
+    @FXML
+    private JFXTextField txtPasswoed;
+
+}
