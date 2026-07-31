1b227a8f9af6efd84d4dbaf805bbd5743348e2ed
ML510 <mr.mahesh.303@gmail.com>
2026-07-31 13:39:46 +0530

    Factory Update

--- src/main/java/repository/DaoFactory.java
+++ src/main/java/repository/DaoFactory.java
@@ -1,5 +1,10 @@
 package repository;
 
+import repository.custom.impl.CustomerDaoImpl;
+import repository.custom.impl.ItemDaoImpl;
+import repository.custom.impl.OrderDaoImpl;
+import util.DaoType;
+
 public class DaoFactory {
 
     private static DaoFactory instance;
@@ -10,5 +15,15 @@ public class DaoFactory {
 
     public static DaoFactory getInstance() {
         return instance ==null ? instance=new DaoFactory() : instance;
+    }
+
+    public <T extends SuperDao> T getDaoType(DaoType type){
+
+        switch (type){
+            case CUSTOMER:return (T) new CustomerDaoImpl();
+            case ITEM:return (T) new ItemDaoImpl();
+            case ORDER:return (T) new OrderDaoImpl();
+        }
+        return null;
     }
 }
--- src/main/java/service/ServiceFactory.java
+++ src/main/java/service/ServiceFactory.java
@@ -1,5 +1,10 @@
 package service;
 
+import service.custom.impl.CustomerServiceImpl;
+import service.custom.impl.ItemServiceImpl;
+import service.custom.impl.OrderServiceImpl;
+import util.ServiceType;
+
 public class ServiceFactory {
     private static ServiceFactory instance;
 
@@ -11,4 +16,13 @@ public class ServiceFactory {
         return instance == null ? instance=new ServiceFactory() : instance;
     }
 
+    public <T extends SuperService> T getServiceType(ServiceType type){
+
+        switch (type){
+            case CUSTOMER:return (T) new CustomerServiceImpl();
+            case ITEM:return (T) new ItemServiceImpl();
+            case ORDER:return (T) new OrderServiceImpl();
+        }
+        return null;
+    }
 }
